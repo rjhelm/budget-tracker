2c7f0734df6f8463baffb5c22fbfff5024139ca4
Ryan Helm <rjhelm@github.io>
2021-06-05 13:47:05 -0600

    db setup for use || still need delete pending function

---
+++ public/db.js
@@ -0,0 +1,52 @@
+const { checkout } = require("../routes/api");
+
+let db;
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    db.createObjectStore("pending", { autoIncrement: true });
+};
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+    // checks for online status before db is read
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+};
+
+request.onerror = function(event) {
+    console.log("Error, Error, Error " + event.target.errorCode);
+};
+
+const saveRecord = (record) => {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    store.add(record);
+}
+
+const checkDatabase = () => {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+
+    getAll.onsuccess = () => {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                header: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            }).then(response => response.json).then(() => {
+                // delete record if succesful
+                const transaction = db.transaction(["pending"], "readwrite");
+                const store = transaction.objectStore("pending");
+                store.clear();
+            });
+        }
+    };
+}
+
