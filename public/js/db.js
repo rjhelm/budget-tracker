48a72f817938aee7b421a4b1b59c06b85a189ddd
Ryan Helm <rjhelm@github.io>
2021-06-05 18:04:35 -0600

    adjusting db.js

--- public/js/db.js
+++ public/js/db.js
@@ -1,60 +1,71 @@
-const { checkout } = require("../../routes/api");
+const indexesDB = 
+window.indexedDB ||
+window.mozIndexDB ||
+window.webkitIndexDB ||
+window.msIndexDB ||
+winbow.shimIndexDB;
 
 let db;
 const request = indexedDB.open("budget", 1);
 
-request.onupgradeneeded = function(event) {
-    const db = event.target.result;
+
+request.onupgradeneeded = ({ target }) => {
+    let db = target.results;
     db.createObjectStore("pending", { autoIncrement: true });
-};
-
-request.onsuccess = function(event) {
-    db = event.target.result;
-    // checks for online status before db is read
-    if (navigator.onLine) {
-        checkDatabase();
-    }
-};
-
-request.onerror = function(event) {
-    console.log("Error, Error, Error " + event.target.errorCode);
-};
-
-const saveRecord = (record) => {
-    const transaction = db.transaction(["pending"], "readwrite");
-    const store = transaction.objectStore("pending");
-    store.add(record);
 }
 
-const checkDatabase = () => {
-    const transaction = db.transaction(["pending"], "readwrite");
-    const store = transaction.objectStore("pending");
-    const getAll = store.getAll();
+// request.onupgradeneeded = function(event) {
+//     const db = event.target.result;
+//     db.createObjectStore("pending", { autoIncrement: true });
+// };
 
-    getAll.onsuccess = () => {
-        if (getAll.result.length > 0) {
-            fetch("/api/transaction/bulk", {
-                method: "POST",
-                body: JSON.stringify(getAll.result),
-                header: {
-                    Accept: "application/json, text/plain, */*",
-                    "Content-Type": "application/json"
-                }
-            }).then(response => response.json).then(() => {
-                // delete record if succesful
-                const transaction = db.transaction(["pending"], "readwrite");
-                const store = transaction.objectStore("pending");
-                store.clear();
-            });
-        }
-    };
-}
+// request.onsuccess = function(event) {
+//     db = event.target.result;
+//     // checks for online status before db is read
+//     if (navigator.onLine) {
+//         checkDatabase();
+//     }
+// };
 
-const deletePending = () => {
-    const transaction = db.transaction(["pending"], "readwrite");
-    const store = transaction.objectStore("pending");
-    store.clear();
-}
+// request.onerror = function(event) {
+//     console.log("Error, Error, Error " + event.target.errorCode);
+// };
 
-// added so that the app knows to listen for connecting online 
-window.addEventListener("online", checkDatabase);
+// const saveRecord = (record) => {
+//     const transaction = db.transaction(["pending"], "readwrite");
+//     const store = transaction.objectStore("pending");
+//     store.add(record);
+// }
+
+// const checkDatabase = () => {
+//     const transaction = db.transaction(["pending"], "readwrite");
+//     const store = transaction.objectStore("pending");
+//     const getAll = store.getAll();
+
+//     getAll.onsuccess = () => {
+//         if (getAll.result.length > 0) {
+//             fetch("/api/transaction/bulk", {
+//                 method: "POST",
+//                 body: JSON.stringify(getAll.result),
+//                 header: {
+//                     Accept: "application/json, text/plain, */*",
+//                     "Content-Type": "application/json"
+//                 }
+//             }).then(response => response.json).then(() => {
+//                 // delete record if succesful
+//                 const transaction = db.transaction(["pending"], "readwrite");
+//                 const store = transaction.objectStore("pending");
+//                 store.clear();
+//             });
+//         }
+//     };
+// }
+
+// const deletePending = () => {
+//     const transaction = db.transaction(["pending"], "readwrite");
+//     const store = transaction.objectStore("pending");
+//     store.clear();
+// }
+
+// // added so that the app knows to listen for connecting online 
+// window.addEventListener("online", checkDatabase);
